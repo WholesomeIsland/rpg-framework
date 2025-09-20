56934857dcf58a91ed2f09d96c4f75c1b7266e97
WholesomeIsland <65096556+WholesomeIsland@users.noreply.github.com>
2025-09-20 00:07:50 -0400

    enemy parser

--- entities/entity.cpp
+++ entities/entity.cpp
@@ -2,6 +2,7 @@
 
 #include "enemy.hpp"
 #include "party.hpp"
+#include "inicpp.hpp"
 
 void Enemy::Attack(Character* c){
     
@@ -9,4 +10,13 @@ void Enemy::Attack(Character* c){
 Enemy::Enemy(std::string spriteFile, std::string enemyDescFile, int lvl){
     sprite = new Sprite(spriteFile);
     doneTurn = false;
+    std::ifstream file(enemyDescFile);
+    ini::IniFile ini(file);
+    this->defense = ini["stats"]["defense"].as<int>();
+    this->maxHealth = ini["stats"]["maxHealth"].as<int>() + (lvl * 2);
+    this->health = this->maxHealth;
+    this->maxMagic = ini["stats"]["maxMagic"].as<int>();
+    this->magic = this->maxMagic;
+    this->speed = ini["stats"]["speed"].as<int>() + (lvl);
+    this->type = static_cast<EnemyType>(ini["stats"]["type"].as<int>());
 }
