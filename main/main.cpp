8df833c0d143b9fbe4bc0c9ff4debe9d2faec83d
Wholesomeisland <spinnerbowl@hotmail.com>
2025-09-29 19:54:56 -0400

    Update main.cpp

--- main/main.cpp
+++ main/main.cpp
@@ -1,7 +1,6 @@
 #include "game.hpp"
-float playerSpeed = 30.f;
 int main(){
-    sf::RenderWindow mainWindow(sf::VideoMode({900, 540}), "Main Window");
+    sf::RenderWindow mainWindow(sf::VideoMode({900, 540}), "rpg-framwork");
     sf::Clock clock;
     Game game;
     
